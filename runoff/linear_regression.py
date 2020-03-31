42aa82566263ebe49d05a6cd38214587698e6017
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-03-31 14:01:17 -0600

    commiting graphs and graphing script

--- runoff/linear_regression.py
+++ runoff/linear_regression.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def graph(filename='', x_axis_title='', y_axis_title=''):
+def graph(filename='', x_axis_data='', y_axis_data='', title='', xlabel='', ylabel='', save_fig=False):
   """
   graph - Graphs the data from the given file
 
@@ -20,10 +20,29 @@ def graph(filename='', x_axis_title='', y_axis_title=''):
   #print(df.head())
 
   plt.figure()
-  sns.scatterplot(x=df[x_axis_title], y=df[y_axis_title])
+  sns.regplot(x=df[x_axis_data], y=df[y_axis_data])
+  plt.title(title)
+  plt.xlabel(xlabel)
+  plt.ylabel(ylabel)
   plt.show()
+
+  if save_fig:
+    plt.savefig('../media/{}_vs_{}.png'.format(xlabel, ylabel))
 
 
 if __name__ == '__main__':
-  graph('runoff_precip_data.csv', 'Snowfall', 'Discharge')
-  graph('runoff_precip_data.csv', 'Precip', 'Discharge')
+  graph(filename='runoff_precip_data.csv', 
+          x_axis_data='Snowfall', 
+          xlabel='Snowfall (inches)', 
+          y_axis_data='Discharge', 
+          ylabel='River Discharge (cubic feet per second)',
+          title='Relationship Between Snowfall and River Discharge in the Crested Butte Basin',
+          save_fig=True)
+
+  graph(filename='runoff_precip_data.csv', 
+          x_axis_data='Precip', 
+          xlabel='Precipitation (inches)', 
+          y_axis_data='Discharge', 
+          ylabel='River Discharge (cubic feet per second)',
+          title='Relationship Between Precipitation and River Discharge in the Crested Butte Basin',
+          save_fig=True)
