32c7535c5e9584e46e4ff967df22c753a8ff45ba
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-03-31 14:13:41 -0600

    corrected graphs

--- runoff/linear_regression.py
+++ runoff/linear_regression.py
@@ -20,14 +20,21 @@ def graph(filename='', x_axis_data='', y_axis_data='', title='', xlabel='', ylab
   #print(df.head())
 
   plt.figure()
-  sns.regplot(x=df[x_axis_data], y=df[y_axis_data])
+
+  # Create a linear regression plot with the given x and y data. Add bands indicating 95% confidence interval (ci)
+  reg = sns.regplot(x=df[x_axis_data], y=df[y_axis_data], ci=95)
+
+  # Set the title of the graph
   plt.title(title)
   plt.xlabel(xlabel)
   plt.ylabel(ylabel)
+
+  # Display the graph
   plt.show()
 
+  # If user indicated saving, save the figure
   if save_fig:
-    plt.savefig('../media/{}_vs_{}.png'.format(xlabel, ylabel))
+    ( reg.get_figure() ).savefig('../media/{}_vs_{}.png'.format(xlabel, ylabel))
 
 
 if __name__ == '__main__':
@@ -36,7 +43,7 @@ if __name__ == '__main__':
           xlabel='Snowfall (inches)', 
           y_axis_data='Discharge', 
           ylabel='River Discharge (cubic feet per second)',
-          title='Relationship Between Snowfall and River Discharge in the Crested Butte Basin',
+          title='Relationship Between Snowfall and River Discharge\nin the Crested Butte Basin',
           save_fig=True)
 
   graph(filename='runoff_precip_data.csv', 
@@ -44,5 +51,5 @@ if __name__ == '__main__':
           xlabel='Precipitation (inches)', 
           y_axis_data='Discharge', 
           ylabel='River Discharge (cubic feet per second)',
-          title='Relationship Between Precipitation and River Discharge in the Crested Butte Basin',
+          title='Relationship Between Precipitation and River Discharge\nin the Crested Butte Basin',
           save_fig=True)
