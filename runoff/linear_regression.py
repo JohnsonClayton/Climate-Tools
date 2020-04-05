e2aa84d3774e976ef6713fba34113b838691f4b9
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-04-05 15:53:22 -0600

    updated with r squared metric

--- runoff/linear_regression.py
+++ runoff/linear_regression.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy
 
 def graph(filename='', x_axis_data='', y_axis_data='', title='', xlabel='', ylabel='', save_fig=False):
   """
@@ -22,7 +23,7 @@ def graph(filename='', x_axis_data='', y_axis_data='', title='', xlabel='', ylab
   plt.figure()
 
   # Create a linear regression plot with the given x and y data. Add bands indicating 95% confidence interval (ci)
-  reg = sns.regplot(x=df[x_axis_data], y=df[y_axis_data], ci=95)
+  reg = sns.regplot(x=df[x_axis_data], y=df[y_axis_data]/12.0, ci=95)
 
   # Set the title of the graph
   plt.title(title)
@@ -36,20 +37,35 @@ def graph(filename='', x_axis_data='', y_axis_data='', title='', xlabel='', ylab
   if save_fig:
     ( reg.get_figure() ).savefig('../media/{}_vs_{}.png'.format(xlabel, ylabel))
 
+def calc_rsquared(filename='', xlabel='', ylabel=''):
+  data = pd.read_csv(filename, index_col=0)
+  slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(data[xlabel], data[ylabel])
+  return r_value**2
+
 
 if __name__ == '__main__':
-  graph(filename='runoff_precip_data.csv', 
+  print('R_squared for Snowfall and Discharge: {}'.format(calc_rsquared(filename='runoff_precip_data1.csv',xlabel='Snowfall', ylabel='Discharge')))
+  graph(filename='runoff_precip_data1.csv', 
           x_axis_data='Snowfall', 
           xlabel='Snowfall (inches)', 
           y_axis_data='Discharge', 
-          ylabel='River Discharge (cubic feet per second)',
+          ylabel='River Discharge (cubic feet)',
           title='Relationship Between Snowfall and River Discharge\nin the Crested Butte Basin',
           save_fig=True)
 
-  graph(filename='runoff_precip_data.csv', 
+  print('R_squared for Precipitation and Discharge: {}'.format(calc_rsquared(filename='runoff_precip_data1.csv',xlabel='Precip', ylabel='Discharge')))
+  graph(filename='runoff_precip_data1.csv', 
           x_axis_data='Precip', 
           xlabel='Precipitation (inches)', 
           y_axis_data='Discharge', 
-          ylabel='River Discharge (cubic feet per second)',
+          ylabel='River Discharge (cubic feet)',
+          title='Relationship Between Precipitation and River Discharge\nin the Crested Butte Basin',
+          save_fig=True)
+  print('R_squared for Precipitation + Snowfall and Discharge: {}'.format(calc_rsquared(filename='runoff_precip_data1.csv',xlabel='Precip/Snowfall', ylabel='Discharge')))
+  graph(filename='runoff_precip_data1.csv', 
+          x_axis_data='Precip/Snowfall', 
+          xlabel='Precipitation and Snowfall (inches)', 
+          y_axis_data='Discharge', 
+          ylabel='River Discharge (cubic feet)',
           title='Relationship Between Precipitation and River Discharge\nin the Crested Butte Basin',
           save_fig=True)
